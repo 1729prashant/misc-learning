33d8283a6bec8f75f9b2ec7ec009d12cfd7c40f0
Prashant V Gaikar <1729prashant@gmail.com>
2024-11-21 00:46:21 +0530

    added structs

---
+++ go_learn/bootdev-lessons/CH004_STRUCTS/006_Struct_methods_in_Go/assignment/main.go
@@ -0,0 +1,13 @@
+package main
+
+type authenticationInfo struct {
+	username string
+	password string
+}
+
+// create the method below
+
+func (authInfo authenticationInfo) getBasicAuth() string {
+	return "Authorization: Basic " + authInfo.username + ":" + authInfo.password
+}
+
